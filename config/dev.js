c9b776f3f574a630906cd23c79365e778b28c52a
Jarvis <kajay1604@gmail.com>
2020-11-20 11:38:23 +0530

    First commit

---
+++ config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongo_uri: "mongodb+srv://user_ajay_crud:jarvis16@cluster0.4nrbf.mongodb.net/mern-crud?retryWrites=true&w=majority"
+}
---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyparser = require("body-parser");
+const keys = require("./config/keys");
+const cors = require("cors");
+
+mongoose.connect(keys.mongo_uri);
+
+const app = express();
+app.use(bodyparser.json());
+app.use(cors());
+
+app.use("/posts", require("./routes/post"))
+
+if(process.env.NODE_ENV=="production") {
+    app.use(express.static("client/build"));
+    const path = require("path");
+    app.get("*", (req, res)=> {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    })
+}
+
+app.get("/", (req, res) => {
+    res.send({Project: 'MERN CRUD updated'});
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
